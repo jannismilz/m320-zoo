68fb7e82187eb94c05c674761c3ef79e57ff1f57
Jannis Milz <jannis@milz.ch>
2023-11-24 11:24:11 +0100

    Make Header fixed to the top

--- app/partials/Header.tsx
+++ app/partials/Header.tsx
@@ -3,7 +3,7 @@ import ProfileLink from "../components/ProfileLink";
 
 export default function Header() {
   return (
-    <div className="flex justify-between bg-red-400 p-5">
+    <div className="fixed top-0 flex min-w-full justify-between bg-red-400 p-5">
       <Logo />
       <ProfileLink avatarUrl="https://picsum.photos/60/60" username="John Doe" />
     </div>
