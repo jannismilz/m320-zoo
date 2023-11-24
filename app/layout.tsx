ee0501b7e3d5a284dae1bc3fb5ff237345de4db7
Jannis Milz <jannis@milz.ch>
2023-11-24 11:11:36 +0100

    Rename Navigation to Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Navigation from "./partials/Navigation";
+import Header from "./partials/Header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -11,7 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <Navigation />
+        <Header />
         {children}
       </body>
     </html>
---
+++ app/partials/Header.tsx
@@ -0,0 +1,11 @@
+import Logo from "../components/Logo";
+import ProfileLink from "../components/ProfileLink";
+
+export default function Header() {
+  return (
+    <nav className="flex justify-between bg-red-400 p-5">
+      <Logo />
+      <ProfileLink avatarUrl="https://picsum.photos/60/60" username="John Doe" />
+    </nav>
+  );
+}
