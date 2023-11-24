4bf07825d27f525c8a720e3dd401fe92bf75d1ed
Jannis Milz <jannis@milz.ch>
2023-11-24 12:58:58 +0100

    Add nav item icons

--- app/components/NavItem.tsx
+++ app/components/NavItem.tsx
@@ -1,9 +1,40 @@
+import {
+  HomeIcon,
+  MapPinIcon,
+  RectangleStackIcon,
+  TicketIcon,
+} from "@heroicons/react/24/outline";
+import {
+  HomeIcon as HomeIconSolid,
+  MapPinIcon as MapPinIconSolid,
+  RectangleStackIcon as RectangleStackIconSolid,
+  TicketIcon as TicketIconSolid,
+} from "@heroicons/react/24/solid";
 import Link from "next/link";
 
-export default function NavItem({ title, route }: { title: string; route: string }) {
+export default function NavItem({ icon, route }: { icon: string; route: string }) {
+  let itemIcon;
+  const width = 30;
+  const height = 30;
+
+  switch (icon) {
+    case "home":
+      itemIcon = <HomeIcon width={width} height={height} />;
+      break;
+    case "tickets":
+      itemIcon = <TicketIcon width={width} height={height} />;
+      break;
+    case "map":
+      itemIcon = <MapPinIcon width={width} height={height} />;
+      break;
+    case "animals":
+      itemIcon = <RectangleStackIcon width={width} height={height} />;
+      break;
+  }
+
   return (
     <li>
-      <Link href={route}>{title}</Link>
+      <Link href={route}>{itemIcon}</Link>
     </li>
   );
 }
--- app/partials/Navigation.tsx
+++ app/partials/Navigation.tsx
@@ -1,13 +1,14 @@
+import { HomeIcon } from "@heroicons/react/24/outline";
 import NavItem from "../components/NavItem";
 
 export default function Navigation() {
   return (
     <nav className="fixed bottom-0 w-full min-w-full bg-green-400">
       <ul className="m-auto flex max-w-lg justify-around p-5">
-        <NavItem title="Test1" route="/" />
-        <NavItem title="Test2" route="/" />
-        <NavItem title="Test3" route="/" />
-        <NavItem title="Test4" route="/" />
+        <NavItem icon="home" route="/" />
+        <NavItem icon="tickets" route="/" />
+        <NavItem icon="map" route="/" />
+        <NavItem icon="animals" route="/" />
       </ul>
     </nav>
   );
