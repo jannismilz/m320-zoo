5820a703fcf87feb6a30e633518c2dd3c8b10225
Jannis Milz <jannis@milz.ch>
2023-11-27 22:45:07 +0100

    Safe user to firestore with timestamps

--- app/contexts/AuthContext.tsx
+++ app/contexts/AuthContext.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 import { createContext, useState } from "react";
-import { auth } from "../firebase/firebaseConfig";
+import { auth, db } from "../firebase/firebaseConfig";
 import { User } from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import firebase from "firebase/compat/app";
+import { TAuthContextProps, TUser } from "../types/types";
 
-type AuthContextProps = {
-    user: User | null;
-};
-
-const defaultValue: AuthContextProps = {
+const defaultValue: TAuthContextProps = {
     user: null,
 };
 
@@ -21,9 +20,24 @@ export function AuthProvider({
 }): JSX.Element | null {
     const [user, setUser] = useState<User | null>(null);
 
-    auth.onAuthStateChanged(function (user) {
+    auth.onAuthStateChanged(async function (user) {
         if (user) {
             setUser(user);
+            const userDoc = await getDoc(doc(db, "users", user.uid));
+
+            const userDataObj: TUser = {
+                displayName: user.displayName,
+                email: user.email,
+                photoURL: user.photoURL,
+                updated_at: Date.now(),
+            };
+
+            if (userDoc.exists()) {
+                setDoc(doc(db, "users", user.uid), userDataObj, { merge: true });
+            } else {
+                userDataObj["created_at"] = Date.now();
+                setDoc(doc(db, "users", user.uid), userDataObj, { merge: true });
+            }
         } else {
             setUser(null);
         }
