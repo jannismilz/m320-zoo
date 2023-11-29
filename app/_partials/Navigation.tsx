babdf481178b1be35d9b3061a8753f14fc443ffd
Jannis Milz <jannis@milz.ch>
2023-11-29 21:18:05 +0100

    Adjust ticket route in navigation bar

--- app/_partials/Navigation.tsx
+++ app/_partials/Navigation.tsx
@@ -6,7 +6,7 @@ export default function Navigation() {
         <nav className="fixed bottom-0 w-full min-w-full bg-green-400">
             <ul className="m-auto flex max-w-lg justify-around p-5">
                 <NavItem icon="home" route="/" />
-                <NavItem icon="tickets" route="/" />
+                <NavItem icon="tickets" route="/tickets" />
                 <NavItem icon="map" route="/" />
                 <NavItem icon="animals" route="/" />
             </ul>
