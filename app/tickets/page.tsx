941632bc1535176fbb2125e7121136699f01d375
Jannis Milz <jannis@milz.ch>
2023-11-29 21:17:56 +0100

    Add tickets purchase page

---
+++ app/tickets/page.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import { useState } from "react";
+import { ETicketType, TTicket } from "../_types/types";
+import TicketAmountSelector from "../_components/TicketAmountSelector";
+import TicketTypeSelector from "../_components/TicketTypeSelector";
+import Input from "../_components/Input";
+import { useAuth } from "../_hooks/useAuth";
+import { addDoc, collection, doc, setDoc } from "firebase/firestore";
+import { db } from "../_firebase/firebaseConfig";
+import TicketDateSelector from "../_components/TicketDateSelector";
+import { useRouter } from "next/navigation";
+
+export default function Tickets() {
+    const { user } = useAuth();
+    const router = useRouter();
+
+    const [date, setDate] = useState("");
+    const [kidTickets, setKidTickets] = useState(0);
+    const [adultTickets, setAdultTickets] = useState(0);
+    const [ticketType, setTicketType] = useState<ETicketType>(ETicketType.DAYPASS);
+    const [next, setNext] = useState(false);
+
+    const [useLoggedInUser, setUseLoggedInUser] = useState(false);
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [email, setEmail] = useState("");
+
+    // TODO: Add purchase email
+
+    async function handlePurchase() {
+        const ticketData: TTicket = {
+            date,
+            kidsAmount: kidTickets,
+            adultsAmount: adultTickets,
+            type: ticketType,
+        };
+
+        if (useLoggedInUser) {
+            ticketData.userId = user?.uid;
+        } else {
+            ticketData.firstName = firstname;
+            ticketData.lastName = lastname;
+            ticketData.email = email;
+        }
+
+        const ticketDoc = await addDoc(collection(db, "tickets"), ticketData);
+        router.push(`/tickets/${ticketDoc.id}`);
+    }
+
+    return (
+        <div>
+            <TicketDateSelector state={date} setState={setDate} />
+            <TicketAmountSelector
+                name="Kids"
+                state={kidTickets}
+                setState={setKidTickets}
+            />
+            <TicketAmountSelector
+                name="Adults"
+                state={adultTickets}
+                setState={setAdultTickets}
+            />
+            <TicketTypeSelector state={ticketType} setState={setTicketType} />
+            <button
+                className={`${next ? "hidden" : "block"}`}
+                onClick={() => setNext(true)}
+            >
+                Next
+            </button>
+            <div className={`${next ? "block" : "hidden"} flex flex-col`}>
+                <hr />
+                <button onClick={() => setUseLoggedInUser(!useLoggedInUser)}>
+                    {useLoggedInUser
+                        ? "Using logged in. Click to enter information manually"
+                        : "Use logged in user"}
+                </button>
+                <div className={`${useLoggedInUser ? "hidden" : "block"} flex flex-col`}>
+                    <Input
+                        name="Firstname"
+                        state={firstname}
+                        setState={setFirstname}
+                        placeholder="Firstname"
+                    />
+                    <Input
+                        name="Lastname"
+                        state={lastname}
+                        setState={setLastname}
+                        placeholder="Lastname"
+                    />
+                    <Input
+                        name="Email"
+                        state={email}
+                        setState={setEmail}
+                        placeholder="Email"
+                        email
+                    />
+                </div>
+                <button onClick={() => handlePurchase()}>Purchase</button>
+            </div>
+        </div>
+    );
+}
