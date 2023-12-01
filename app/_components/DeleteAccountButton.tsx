6760fb27342e9b952c554c56889da21a4f30633c
Jannis Milz <jannis@milz.ch>
2023-12-01 08:53:01 +0100

    Add account deletion confirmation modal

--- app/_components/DeleteAccountButton.tsx
+++ app/_components/DeleteAccountButton.tsx
@@ -1,15 +1,13 @@
 import { User, signOut } from "firebase/auth";
 import { deleteDoc, doc, getDoc, getDocs, query, where } from "firebase/firestore";
-import {
-    auth,
-    db,
-    ticketsCollection,
-    usersCollection,
-} from "../_firebase/firebaseConfig";
+import { auth, ticketsCollection, usersCollection } from "../_firebase/firebaseConfig";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Modal, ModalContent, ModalHeader, ModalFooter, Button } from "@nextui-org/react";
 
 export default function DeleteAccountButton({ user }: { user: User }) {
     const router = useRouter();
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     async function handleDeleteAccount() {
         await signOut(auth);
@@ -26,5 +24,32 @@ export default function DeleteAccountButton({ user }: { user: User }) {
         router.push("/");
     }
 
-    return <button onClick={() => handleDeleteAccount()}>Delete Account</button>;
+    return (
+        <div>
+            <button onClick={() => setIsModalOpen(true)}>Delete Account</button>
+            <Modal size="sm" isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+                <ModalContent>
+                    {(onClose) => (
+                        <>
+                            <ModalHeader className="flex flex-col gap-1">
+                                Are you sure you want to permanently delete your account?
+                            </ModalHeader>
+                            <ModalFooter>
+                                <Button color="default" onPress={onClose}>
+                                    Close
+                                </Button>
+                                <Button
+                                    color="danger"
+                                    variant="light"
+                                    onPress={() => handleDeleteAccount()}
+                                >
+                                    Delete
+                                </Button>
+                            </ModalFooter>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
+        </div>
+    );
 }
